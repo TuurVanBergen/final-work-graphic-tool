bcd52d34e9f6a61f560d55235dd942c4f00d8a00
TuurVanBergen <tuur.van.bergen@student.ehb.be>
2025-05-31 15:48:29 +0200

    feat(tool2): added slider config file

---
+++ frontend/src/config/SLIDER_CONFIG_TOOL2.js
@@ -0,0 +1,8 @@
+export const SLIDER_CONFIG_TOOL2 = [
+	{ id: "slider1", label: "Frame Top", min: 0, max: 3, step: 1 },
+	{ id: "slider2", label: "Frame Middle", min: 0, max: 3, step: 1 },
+	{ id: "slider3", label: "Frame Bottom", min: 0, max: 3, step: 1 },
+	{ id: "slider4", label: "Small Top", min: 0, max: 3, step: 1 },
+	{ id: "slider5", label: "Small Bottom", min: 0, max: 3, step: 1 },
+	{ id: "slider6", label: "Small Lower Top", min: 0, max: 3, step: 1 },
+];
