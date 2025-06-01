78e148441540e872f94569a998f16e0af891dc3f
TuurVanBergen <tuur.van.bergen@student.ehb.be>
2025-06-01 13:22:56 +0200

    feat(postercanvas, sliders): load slider settings from external config file

---
+++ frontend/src/config/SLIDER_CONFIG_POSTER.js
@@ -0,0 +1,44 @@
+export const SLIDER_CONFIG_POSTER = [
+	{
+		id: "scale",
+		label: "Schaal",
+		min: 0.1,
+		max: 3,
+		step: 0.01,
+	},
+	{
+		id: "fillHue",
+		label: "Voorgrondkleur",
+		min: 0,
+		max: 9,
+		step: 1,
+	},
+	{
+		id: "bgHue",
+		label: "Achtergrondkleur",
+		min: 0,
+		max: 9,
+		step: 1,
+	},
+	{
+		id: "texture",
+		label: "Textuur",
+		min: 0,
+		max: 10,
+		step: 1,
+	},
+	{
+		id: "inkBleed",
+		label: "Ink Bleed",
+		min: 0,
+		max: 20,
+		step: 1,
+	},
+	{
+		id: "blend",
+		label: "Blend",
+		min: 0,
+		max: 100,
+		step: 1,
+	},
+];
